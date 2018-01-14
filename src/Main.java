84bcbd660694f1966be330fdf7bbb7b190d4a93a
marek magnuszewski <marek.magnuszewski@wp.pl>
2018-01-14 12:27:00 +0100

    Add GUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
+    private JTextField text;
+    private JButton addObjectButton;
+    private JPanel jPanel;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main main=new Main();
+
+        JFrame jFrame=new JFrame();
+        jFrame.setContentPane(main.jPanel);
+        jFrame.setSize(300,300);
+        
+        jFrame.setLocationRelativeTo(jFrame);
+        jFrame.setVisible(true);
+
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+       //System.out.println("Hello World!");
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
